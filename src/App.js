852736451ba580f7b25dd8191b4b0f1c105996e4
linhnph <nphlinh26799@gmail.com>
2022-01-27 02:48:13 +0700

    add login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import {Login} from './Components/Auth/Login'
 function App() {
   return (
     <div className="App">
-     
+     <Login></Login>
     </div>
   );
 }
--- src/Components/Auth/Login.jsx
+++ src/Components/Auth/Login.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-
+import './Login.css'
 export const Login = () => {
   return (
-      <>
-      </>
+    <>
+      <form className='form-login'>
+        <div className='circle-drop'></div>
+        <div className='header'>
+          <div className='title'>Welcome</div>
+          <div className='title'>Back</div>
+          <div className='content'>Please enter information to continue.</div>
+        </div>
+        <div className='body'>
+          <div className='input'>
+            <input type="text" placeholder='Email' />
+          </div>
+          <div className='input'>
+            <input type="password" placeholder='Password' />
+          </div>
+          <div className='link-text'>
+            Forget your password?
+          </div>
+          <input type="button" className='round-button sign-in' value='Sign In' />
+          <small className='break-line'>Or</small>
+          <input type="button" className='round-button sign-up' value='Sign Up' />
+        </div>
+      </form>
+    </>
   );
 };
